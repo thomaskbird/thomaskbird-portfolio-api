4fe9bbd60233e9c0bdefa0897f380e996e47695c
Thomas Bird <tbird@control-tec.com>
2018-09-04 23:59:55 -0400

    Fixing various issues

--- resources/views/structure/header-public.blade.php
+++ resources/views/structure/header-public.blade.php
@@ -15,28 +15,7 @@
 
         <nav class="collapse navbar-collapse menu">
             <ul id="menu-thomasbird" class="nav navbar-nav navbar-right">
-                @foreach($navigation as $nav)
-                    <li @if(Request::route()->getName() === $nav['route']) class="active" @endif >
-                        <a title="{{ $nav['text'] }}" href="{{ route($nav['route'], $nav['route_params']) }}">
-                            <span class="glyphicon glyphicon-{{ $nav['icon'] }}"></span>
-                            {{ ucfirst($nav['text']) }}
-                        </a>
-                    </li>
-                @endforeach
             </ul></nav>
     </div><!-- /.container-fluid -->
 
 </nav>
-
-<div id="twitter-feed">
-    <div class="container">
-        <b>Whats new:</b> Currently open to new fulltime, part time and contract roles
-    </div>
-</div>
-
-@if( Session::has('alert') )
-    <div class="alert alert-{{ Session::get('alert.class') }} alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {{ Session::get('alert.msg') }}
-    </div>
-@endif
